d65c00f5ed17554a1ef6a56bb86064fe0c1a03c7
dev Kacha <kachadev60@gmail.com>
2026-01-20 10:40:39 +0530

    first step

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+    return new PrismaClient();
+};
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") {
+    globalThis.prisma = prisma;
+}
